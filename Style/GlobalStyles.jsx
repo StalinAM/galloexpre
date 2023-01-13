eec294c5f38e4c659645f4e5f66fa485c522a683
Stalin <sta_vla411@hotmail.com>
2023-01-13 16:41:10 -0500

    add Global styles

---
+++ Style/GlobalStyles.jsx
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components'
+const GlobalStyles = createGlobalStyle`
+    *,*::before,*::after{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    html{
+        scroll-behavior: smooth;
+    }
+    body{
+        overflow-x: hidden;
+        font-family: 'Poppins', sans-serif;
+    }
+    h1,h2,h3,h4,h5,h6{
+        margin: 0;
+        padding: 0;
+    }
+    ul{
+        list-style: none;
+    }
+    a{
+        color: inherit;
+        text-decoration: none;
+        cursor: pointer;
+    }
+    button{
+        cursor: pointer;
+        border: none;
+        outline: none;
+    }
+`
+export default GlobalStyles
