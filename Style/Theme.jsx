aa4dd34b6e23e16780ab7736f7c27d29c7a4c364
Stalin <sta_vla411@hotmail.com>
2023-01-13 22:07:07 -0500

    update colors

--- Style/Theme.jsx
+++ Style/Theme.jsx
@@ -1,8 +1,9 @@
 export const DarkTheme = {
   Title: 'rgb(255, 193, 5)',
-  Subtitle: '#FFFFFF',
-  Description: '#ABB2BF',
-  Background: '#282C33',
+  Subtitle: 'hsl(0, 0%, 0%)',
+  Description: 'hsl(0, 0%, 48%)',
+  Background: '#011883',
+  Background2: '#606e85',
   WidthSection: '65rem',
   Hover: 'hsl(45deg 100% 51% / 20%)'
 }
