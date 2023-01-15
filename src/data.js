3ccc9ba5c26a50477dc632353013fb317a636a42
Stalin <sta_vla411@hotmail.com>
2023-01-14 23:58:12 -0500

    update de js a json

--- src/data.js
+++ src/data.js
@@ -4,37 +4,5 @@ const listMenu = [
   { id: 3, link: '#servicios', title: 'servicios' },
   { id: 4, link: '#contacto', title: 'contacto' }
 ]
-const nosotros = [
-  {
-    id: 1,
-    name: 'visión',
-    description:
-      'Ofrecer un servicio de transporte de excelencia, calidad y oportunidad de trabajo para la sociedad, basado en la satisfacción de las necesidades  de nuestros clientes. Capacitando permanentemente a nuestro personal para brindar un servicio  rápido y competitivo. Operando en forma eficiente, segura y con tecnología de punta.'
-  },
-  {
-    id: 2,
-    name: 'reseña histórica',
-    description:
-      'Esta compañía nace por la necesidad de trabajo y empeño de un grupo de colaboradores quienes un 3 de octubre de 2006, motivados por el deseo de cumplir un sueño, buscan emprender en el mundo de la transportación pesada creando un logotipo inicial de "TRANSPORTE GALLITO” nombre que daría inicio al nacimiento de algo más grande, por medio del cual ingresamos a muchas empresas de renombre, quienes nos dieron la oportunidad de ejercer nuestras aspiraciones de crecimiento personal, familiar y profesional de manera honrada y responsable. Gracias a Dios y a nuestros fieles clientes que creyeron en nuestras capacidades, habilidades y destrezas, a mediados del 2013, nace la compañía jurídica familiar cuyo nombre es "GALLOEXPRE C.A." más sólida pues somos un equipo que busca seguir creciendo para cumplir las expectativas de nuestros clientes y de la compañía en general. Teniendo como principal objetivo ser útil y servir a la sociedad de manera honrada y responsable.'
-  }
-]
-const servicios = [
-  {
-    id: 1,
-    description: 'Disponemos de transporte nuevo, tipo furgones de toda medida.'
-  },
-  {
-    id: 2,
-    description: 'Plataformas y tráiler para peso liviano y extra pesado.'
-  },
-  { id: 3, description: 'Unidades de transporte con rastreo satelital.' },
-  { id: 4, description: 'Camiones para peso más liviano.' },
-  { id: 5, description: 'Personal altamente capacitado para la manipulación' },
-  {
-    id: 6,
-    description:
-      'Disponemos de Instalaciones propias en las ciudades de Quito y Guayaquil'
-  }
-]
 
-export { listMenu, servicios }
+export { listMenu }
