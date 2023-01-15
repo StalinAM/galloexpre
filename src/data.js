13edbc64c6b5fe152f5f191801e4bcd2fe8daeee
Stalin <sta_vla411@hotmail.com>
2023-01-14 20:29:29 -0500

    add information

--- src/data.js
+++ src/data.js
@@ -4,4 +4,18 @@ const listMenu = [
   { id: 3, link: '#servicios', title: 'servicios' },
   { id: 4, link: '#contacto', title: 'contacto' }
 ]
+const nosotros = [
+  {
+    id: 1,
+    name: 'visión',
+    description:
+      'Ofrecer un servicio de transporte de excelencia, calidad y oportunidad de trabajo para la sociedad, basado en la satisfacción de las necesidades  de nuestros clientes. Capacitando permanentemente a nuestro personal para brindar un servicio  rápido y competitivo. Operando en forma eficiente, segura y con tecnología de punta.'
+  },
+  {
+    id: 2,
+    name: 'reseña histórica',
+    description:
+      'Esta compañía nace por la necesidad de trabajo y empeño de un grupo de colaboradores quienes un 3 de octubre de 2006, motivados por el deseo de cumplir un sueño, buscan emprender en el mundo de la transportación pesada creando un logotipo inicial de "TRANSPORTE GALLITO” nombre que daría inicio al nacimiento de algo más grande, por medio del cual ingresamos a muchas empresas de renombre, quienes nos dieron la oportunidad de ejercer nuestras aspiraciones de crecimiento personal, familiar y profesional de manera honrada y responsable. Gracias a Dios y a nuestros fieles clientes que creyeron en nuestras capacidades, habilidades y destrezas, a mediados del 2013, nace la compañía jurídica familiar cuyo nombre es "GALLOEXPRE C.A." más sólida pues somos un equipo que busca seguir creciendo para cumplir las expectativas de nuestros clientes y de la compañía en general. Teniendo como principal objetivo ser útil y servir a la sociedad de manera honrada y responsable.'
+  }
+]
 export { listMenu }
