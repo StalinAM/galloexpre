092f4f98a623dd01186a5311cefbfab7f77ee74b
Stalin <sta_vla411@hotmail.com>
2023-01-15 16:44:42 -0500

    added all sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Home from '../components/Home'
+import Services from '../components/Services'
 import GlobalStyles from '../Style/GlobalStyles'
 import { DarkTheme } from '../Style/Theme'
 
+const Container = styled.main`
+  padding: 0 9rem; ;
+`
 function App() {
   return (
     <>
       <GlobalStyles />
       <ThemeProvider theme={DarkTheme}>
         <Header />
+        <Container>
+          <Home />
+          <About />
+          <Services />
+          <Contact />
+          <Footer />
+        </Container>
       </ThemeProvider>
     </>
   )
