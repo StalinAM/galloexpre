7c576c0484784f28e0dd18a714f4bbb6bf6d3584
Stalin <sta_vla411@hotmail.com>
2023-01-17 11:00:05 -0500

    add contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+import Form from './Form'
 
+const Container = styled.section`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  padding: 0 6rem;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  background-color: ${(props) => props.theme.Backgrounds};
+`
+const Title = styled.h2`
+  font-size: 2rem;
+`
 function Contact() {
   return (
-    <div>Contact</div>
+    <Container id='contacto'>
+      <Title>Contáctenos</Title>
+      <Form />
+    </Container>
   )
 }
 
