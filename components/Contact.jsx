8820c1b578db99204fc5275f2fe053f28d4f7750
Stalin <sta_vla411@hotmail.com>
2023-01-18 16:24:54 -0500

    added direction

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import Direction from './Direction'
 import Form from './Form'
+import data from '../src/assets/data.json'
 
 const Container = styled.section`
   display: flex;
   gap: 2rem;
-  align-items: center;
+  align-items: flex-start;
   padding: 0 6rem;
   min-height: 100vh;
   flex-direction: column;
@@ -15,11 +17,37 @@ const Container = styled.section`
 const Title = styled.h2`
   font-size: 2rem;
 `
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 1rem;
+  align-items: flex-start;
+  justify-content: space-between;
+`
+
 function Contact() {
+  const dirQuito = data.direccion[0]
+  const dirGuayaquil = data.direccion[1]
   return (
     <Container id='contacto'>
       <Title>Contáctenos</Title>
-      <Form />
+      <Content>
+        <Direction
+          ciudad={dirQuito.ciudad}
+          direccion={dirQuito.direccion}
+          celular={dirQuito.celular}
+          email={dirQuito.correo}
+          map={dirQuito.map}
+        />
+        <Form />
+        <Direction
+          ciudad={dirGuayaquil.ciudad}
+          direccion={dirGuayaquil.direccion}
+          celular={dirGuayaquil.celular}
+          email={dirGuayaquil.correo}
+          map={dirGuayaquil.map}
+        />
+      </Content>
     </Container>
   )
 }
