c53f73538dbdfcb2b0f6d2f9c0ccbc0b40c1c045
Stalin <sta_vla411@hotmail.com>
2023-01-21 18:33:45 -0500

    add footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 import styled from 'styled-components'
 import logo from '../src/assets/logoGe.webp'
 const Container = styled.footer`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1.5rem;
+  justify-items: center;
   padding: 2rem 6rem;
 `
 const Img = styled.img`
   width: 12rem;
   height: auto;
   object-fit: cover;
+`
+const Social = styled.ul`
+  display: flex;
+  gap: 1rem;
+`
+const Icon = styled.li`
+  font-size: 1.5rem;
 `
 function Footer() {
   return (
@@ -18,18 +29,21 @@ function Footer() {
           Nos preocupamos por el seguimiento y cuidado que debe darse a su
           mercadería
         </p>
-        <ul>
-          <li>
-            <a href=''>
+        <Social>
+          <Icon>
+            <a href='https://www.facebook.com/GalloExpre-CA-Compa%C3%B1ia-de-Transporte-Pesado-101126645601053'>
               <i className='uil uil-facebook-f' />
             </a>
-          </li>
-          <li>
-            <a href=''>
+          </Icon>
+          <Icon>
+            <a href='https://wa.me/593984662435?text=Me%20ayudas%20con%20información%20'>
               <i className='uil uil-whatsapp' />
             </a>
-          </li>
-        </ul>
+          </Icon>
+        </Social>
+      </div>
+      <div>
+        <h3>Contacto</h3>
       </div>
     </Container>
   )
