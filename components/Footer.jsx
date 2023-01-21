565047f61576a314dba4701c789d78aa890ae4b8
Stalin <sta_vla411@hotmail.com>
2023-01-20 20:41:06 -0500

    inprocess

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import logo from '../src/assets/logoGe.webp'
+const Container = styled.footer`
+  padding: 2rem 6rem;
+`
+const Img = styled.img`
+  width: 12rem;
+  height: auto;
+  object-fit: cover;
+`
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <Container>
+      <div>
+        <Img src={logo} alt='logo Gallo Expre' />
+        <p>
+          Nos preocupamos por el seguimiento y cuidado que debe darse a su
+          mercadería
+        </p>
+        <ul>
+          <li>
+            <a href=''>
+              <i className='uil uil-facebook-f' />
+            </a>
+          </li>
+          <li>
+            <a href=''>
+              <i className='uil uil-whatsapp' />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </Container>
+  )
 }
 
 export default Footer
