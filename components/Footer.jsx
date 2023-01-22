3cafab65ec5e0cd8b33de2dc488c8c51f5d5e795
Stalin <sta_vla411@hotmail.com>
2023-01-22 11:51:59 -0500

    added target an rel

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -15,7 +15,20 @@ const Img = styled.img`
   height: auto;
   object-fit: cover;
 `
+const Menu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.7rem;
+  font-size: 1.1rem;
+`
+const Link = styled.li`
+  &:hover {
+    color: #ff0000;
+  }
+`
 const Social = styled.ul`
+  margin-top: 0.7rem;
   display: flex;
   gap: 1rem;
   color: ${(props) => props.theme.Title};
@@ -31,6 +44,11 @@ const Left = styled.div`
   flex-direction: column;
   align-items: center;
 `
+const Contacto = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.7rem;
+`
 function Footer() {
   return (
     <Container>
@@ -40,22 +58,50 @@ function Footer() {
           Nos preocupamos por el seguimiento y cuidado que debe darse a su
           mercadería.
         </Text>
+      </Left>
+      <div>
+        <Menu>
+          <Link>
+            <a href='#inicio'>Inicio</a>
+          </Link>
+          <Link>
+            <a href='#nosotros'>Nosotros</a>
+          </Link>
+          <Link>
+            <a href='#servicios'>Servicios</a>
+          </Link>
+        </Menu>
         <Social>
           <Icon>
-            <a href='https://www.facebook.com/GalloExpre-CA-Compa%C3%B1ia-de-Transporte-Pesado-101126645601053'>
+            <a
+              href='https://www.facebook.com/GalloExpre-CA-Compa%C3%B1ia-de-Transporte-Pesado-101126645601053'
+              target='_blank'
+              rel='noreferrer'
+            >
               <i className='uil uil-facebook-f' />
             </a>
           </Icon>
           <Icon>
-            <a href='https://wa.me/593984662435?text=Me%20ayudas%20con%20información%20'>
+            <a
+              href='https://wa.me/593984662435?text=Me%20ayudas%20con%20información%20'
+              target='_blank'
+              rel='noreferrer'
+            >
               <i className='uil uil-whatsapp' />
             </a>
           </Icon>
         </Social>
-      </Left>
-      <div>
-        <h3>Contacto</h3>
       </div>
+      <Contacto>
+        <h3>Contácto</h3>
+        <p>
+          Amparo Gallo: <span>099 243 4904</span>
+        </p>
+        <p>
+          William Chasi: <span>099 588 9577 / 098 466 2435</span>
+        </p>
+        <p>operaciones1galloexpre@gmail.com</p>
+      </Contacto>
     </Container>
   )
 }
